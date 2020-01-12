32b32af3477afc131b3b05c16086e911a6bae097
Julien Deniau <julien.deniau@gmail.com>
2020-01-12 11:39:49 +0100

    start typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,14 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: ['airbnb', 'plugin:prettier/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
     'react/jsx-filename-extension': 'off',
     'react/jsx-one-expression-per-line': 'off',
